1385d0ed4df1fc6891d02d6c2549bb5f529444c0
Simya <simya.s@codilar.com>
2022-08-02 15:48:49 +0530

    customer logged in condition added

--- Simya/Assignment6/ViewModel/CatalogData.php
+++ Simya/Assignment6/ViewModel/CatalogData.php
@@ -5,14 +5,15 @@ namespace Simya\Assignment6\ViewModel;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Catalog\Api\Data\ProductInterfaceFactory;
 use Magento\Directory\Model\Currency;
-
+use Magento\Customer\Model\Session;
 
 class CatalogData implements ArgumentInterface
 {
-    public function __construct(ProductInterfaceFactory $productFactory, Currency $currency)
+    public function __construct(ProductInterfaceFactory $productFactory, Currency $currency, Session $session)
     {
         $this->productFactory = $productFactory;
         $this->currency = $currency;
+        $this->session = $session;
     }
 
     public function getProductPrice($id)
@@ -29,6 +30,13 @@ class CatalogData implements ArgumentInterface
     public function getCurrentCurrencySymbol()
     {
         return $this->currency->getCurrencySymbol();
+    }
+    public function getIsLoggedIn()
+    {
+        if ($this->session->isLoggedIn()) {
+            return true;
+        }
+        return false;
     }
 
 }
